cd2ef20187485ccfd41d9cb677f4cca9884ae478
Dmitry Kalinin <6912915+lwinch2006@users.noreply.github.com>
2020-02-09 19:20:49 +0100

    Initial commit

---
+++ OrderService/OrderService.Tests/OrderTests.cs
@@ -0,0 +1,62 @@
+using System.Globalization;
+using NUnit.Framework;
+
+namespace OrderService.Tests
+{
+    [TestFixture]
+    public class OrderTests
+    {
+        private static readonly Product MotorSuper = new Product("Car Insurance", "Super", Product.Prices.TwoThousand);
+        private static readonly Product MotorBasic = new Product("Car Insurance", "Basic", Product.Prices.OneThousand);
+
+        [Test]
+        public void can_generate_html_receipt_for_motor_basic()
+        {
+            var order = new Order("Test Company");
+            order.AddLine(new OrderLine(MotorBasic, 1));
+            var actual = order.GenerateHtmlReceipt();
+
+            var expected =
+                $"<html><body><h1>Order receipt for 'Test Company'</h1><ul><li>1 x Car Insurance Basic = kr 1{NumberFormatInfo.CurrentInfo.NumberGroupSeparator}000,00</li></ul><h3>Subtotal: kr 1{NumberFormatInfo.CurrentInfo.NumberGroupSeparator}000,00</h3><h3>MVA: kr 250,00</h3><h2>Total: kr 1{NumberFormatInfo.CurrentInfo.NumberGroupSeparator}250,00</h2></body></html>";
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void can_generate_html_receipt_for_motor_super()
+        {
+            var order = new Order("Test Company");
+            order.AddLine(new OrderLine(MotorSuper, 1));
+            var actual = order.GenerateHtmlReceipt();
+
+            var expected =
+                $"<html><body><h1>Order receipt for 'Test Company'</h1><ul><li>1 x Car Insurance Super = kr 2{NumberFormatInfo.CurrentInfo.NumberGroupSeparator}000,00</li></ul><h3>Subtotal: kr 2{NumberFormatInfo.CurrentInfo.NumberGroupSeparator}000,00</h3><h3>MVA: kr 500,00</h3><h2>Total: kr 2{NumberFormatInfo.CurrentInfo.NumberGroupSeparator}500,00</h2></body></html>";
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void can_generate_receipt_for_motor_basic()
+        {
+            var order = new Order("Test Company");
+            order.AddLine(new OrderLine(MotorBasic, 1));
+            var actual = order.GenerateReceipt();
+            var expected =
+                $"Order receipt for 'Test Company'\r\n\t1 x Car Insurance Basic = kr 1{NumberFormatInfo.CurrentInfo.NumberGroupSeparator}000,00\r\nSubtotal: kr 1{NumberFormatInfo.CurrentInfo.NumberGroupSeparator}000,00\r\nMVA: kr 250,00\r\nTotal: kr 1{NumberFormatInfo.CurrentInfo.NumberGroupSeparator}250,00";
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void can_generate_receipt_for_motor_super()
+        {
+            var order = new Order("Test Company");
+            order.AddLine(new OrderLine(MotorSuper, 1));
+            var actual = order.GenerateReceipt();
+            var expected =
+                $"Order receipt for 'Test Company'\r\n\t1 x Car Insurance Super = kr 2{NumberFormatInfo.CurrentInfo.NumberGroupSeparator}000,00\r\nSubtotal: kr 2{NumberFormatInfo.CurrentInfo.NumberGroupSeparator}000,00\r\nMVA: kr 500,00\r\nTotal: kr 2{NumberFormatInfo.CurrentInfo.NumberGroupSeparator}500,00";
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# OrderService
+
+Thanks for taking the time to do this refactoring exercise!
+
+## Instructions
+
+This solution contains some classes used by an imaginary service to generate order receipts, and some unit tests to prove that everything works.
+
+Pretend this code is part of a larger software system and you are now responsible for it.
+
+The company has requested these following changes:
+* different discount types and percentages
+* receipt in json format
+
+Refactor the code so that it can survive these changes.
+
+### Tip
+* Your code should trend towards being SOLID.
+* You should be confident that it works. 
+* You should be comfortable the next developer will easily understand how to work with it.
+* We use [NUnit3](http://www.nunit.org) and [FakeItEasy](https://fakeiteasy.github.io/) at Waypoint. References have been added using NuGet.
+
+Please note:
+* There is no right or wrong solution(s) for this task. Show us what you can do and we will discuss your refactoring choices during the technical interview. 
+* Our email system will reject any zip-files/archives that contain binary files, so please do not include any in the solution you send back to us. 
+
+Good luck and, most importantly, have fun :)
